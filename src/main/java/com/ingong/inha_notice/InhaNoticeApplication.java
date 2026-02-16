755f28e17572a5c592b0e4eb3a154a24d3fa1eee
logicallaw <logicallawbio@gmail.com>
2026-02-16 22:20:36 +0900

    docs(#1): update gitignore, LICENSE

---
+++ src/main/java/com/ingong/inha_notice/InhaNoticeApplication.java
@@ -0,0 +1,24 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the Apache License 2.0.
+ * Copyright (c) 2026 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * http://www.apache.org/licenses/
+ * Author: Junho Kim
+ * Latest Updated Date: 2026-02-08
+ */
+
+package com.ingong.inhanotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class InhanoticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InhanoticeApplication.class, args);
+  }
+}
