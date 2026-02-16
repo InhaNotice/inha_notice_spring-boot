b9bcb5456ed28288897711d1046ca1cdb83c795e
logicallaw <logicallawbio@gmail.com>
2026-02-16 22:28:45 +0900

    feat(#1/api): add JoinRequest, LoginRequest

---
+++ src/main/java/com/ingong/inha_notice/api/v1/auth/dto/local/JoinRequest.java
@@ -0,0 +1,31 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the MIT License.
+ * Copyright (c) 2025-2026 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: Junho Kim
+ * Latest Updated Date: 2026-02-16
+ */
+
+package com.ingong.inha_notice.api.v1.auth.dto.local;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record JoinRequest(
+    @NotBlank
+    @Email
+    String email,
+
+    @NotBlank
+    String password,
+
+    @NotNull
+    @AssertTrue
+    Boolean isPrivacyAgreed
+) {
+
+}
---
+++ src/main/java/com/ingong/inha_notice/api/v1/auth/dto/local/LoginRequest.java
@@ -0,0 +1,25 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the MIT License.
+ * Copyright (c) 2025-2026 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: Junho Kim
+ * Latest Updated Date: 2026-02-16
+ */
+
+package com.ingong.inha_notice.api.v1.auth.dto.local;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+    @NotBlank
+    @Email
+    String email,
+
+    @NotBlank
+    String password
+) {
+
+}
