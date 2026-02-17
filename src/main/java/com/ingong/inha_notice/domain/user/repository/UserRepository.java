bd8db90e17580b226e68bcba4abc2a8b75ea038f
logicallaw <logicallawbio@gmail.com>
2026-02-17 23:54:40 +0900

    feat(#1/user): add UserRepository

---
+++ src/main/java/com/ingong/inha_notice/domain/user/repository/UserRepository.java
@@ -0,0 +1,24 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the MIT License.
+ * Copyright (c) 2025-2026 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: Junho Kim
+ * Latest Updated Date: 2026-02-17
+ */
+
+package com.ingong.inha_notice.domain.user.repository;
+
+import com.ingong.inha_notice.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
+
+  boolean existsByEmail(String email);
+
+  Optional<User> findByPublicId(String publicId);
+}
