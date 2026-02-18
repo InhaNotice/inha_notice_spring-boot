63af9bfabf78dd1dd96afcb0a28e53faed4040db
logicallaw <logicallawbio@gmail.com>
2026-02-18 21:54:09 +0900

    feat(#1/user): UserInfoResponseDTO -> UserDTO

--- src/main/java/com/ingong/inha_notice/api/v1/user/dto/UserDTO.java
+++ src/main/java/com/ingong/inha_notice/api/v1/user/dto/UserDTO.java
@@ -5,21 +5,23 @@
  * For full license text, see the LICENSE file in the root directory or at
  * https://opensource.org/license/mit
  * Author: Junho Kim
- * Latest Updated Date: 2026-02-17
+ * Latest Updated Date: 2026-02-18
  */
 
-package com.ingong.inha_notice.api.v1.user.dto.response;
+package com.ingong.inha_notice.api.v1.user.dto;
 
 import com.ingong.inha_notice.domain.user.entity.User;
+import com.ingong.inha_notice.domain.user.enums.UserRole;
 import com.ingong.inha_notice.domain.user.enums.UserStatus;
 
-public record UserInfoResponseDTO(
+public record UserDTO(
     String publicId,
     String email,
-    UserStatus status
+    UserStatus status,
+    UserRole role
 ) {
 
-  public static UserInfoResponseDTO from(User user) {
-    return new UserInfoResponseDTO(user.getPublicId(), user.getEmail(), user.getStatus());
+  public static UserDTO from(User user) {
+    return new UserDTO(user.getPublicId(), user.getEmail(), user.getStatus(), user.getRole());
   }
 }
