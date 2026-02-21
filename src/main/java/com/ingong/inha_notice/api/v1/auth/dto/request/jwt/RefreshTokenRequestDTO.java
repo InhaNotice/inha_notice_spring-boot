94e611453a61ca4aa633151ead1ff8d92d01cd62
logicallaw <logicallawbio@gmail.com>
2026-02-21 22:13:50 +0900

    feat(#1/auth): add RefreshTokenRequestDTO

---
+++ src/main/java/com/ingong/inha_notice/api/v1/auth/dto/request/jwt/RefreshTokenRequestDTO.java
@@ -0,0 +1,22 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the MIT License.
+ * Copyright (c) 2025-2026 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: Junho Kim
+ * Latest Updated Date: 2026-02-21
+ */
+
+package com.ingong.inha_notice.api.v1.auth.dto.request.jwt;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RefreshTokenRequestDTO(
+    @NotBlank
+    String refreshToken,
+    @NotBlank
+    String deviceId
+) {
+
+}
