4675d2904fd204ccc7d3324a6de9728ccbeaa653
logicallaw <logicallawbio@gmail.com>
2026-02-21 22:19:34 +0900

    feat(#1/auth): add /refresh, /logout routes

--- src/main/java/com/ingong/inha_notice/api/v1/auth/AuthApi.java
+++ src/main/java/com/ingong/inha_notice/api/v1/auth/AuthApi.java
@@ -5,19 +5,25 @@
  * For full license text, see the LICENSE file in the root directory or at
  * https://opensource.org/license/mit
  * Author: Junho Kim
- * Latest Updated Date: 2026-02-18
+ * Latest Updated Date: 2026-02-21
  */
 
 package com.ingong.inha_notice.api.v1.auth;
 
-import com.ingong.inha_notice.api.v1.auth.dto.local.request.JoinRequestDTO;
-import com.ingong.inha_notice.api.v1.auth.dto.local.request.LoginRequestDTO;
-import com.ingong.inha_notice.api.v1.auth.dto.local.response.JoinResponseDTO;
-import com.ingong.inha_notice.api.v1.auth.dto.local.response.LoginResponseDTO;
-import com.ingong.inha_notice.global.response.dto.ApiResponseDTO;
+import com.ingong.inha_notice.api.v1.auth.dto.request.jwt.RefreshTokenRequestDTO;
+import com.ingong.inha_notice.api.v1.auth.dto.request.local.JoinRequestDTO;
+import com.ingong.inha_notice.api.v1.auth.dto.request.local.LoginRequestDTO;
+import com.ingong.inha_notice.api.v1.auth.dto.request.local.LogoutRequestDTO;
+import com.ingong.inha_notice.api.v1.auth.dto.response.local.JoinResponseDTO;
+import com.ingong.inha_notice.api.v1.auth.dto.response.local.LoginResponseDTO;
+import com.ingong.inha_notice.global.api.dto.ApiResponseDTO;
+import com.ingong.inha_notice.global.security.auth.AuthenticatedUser;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,4 +39,17 @@ public interface AuthApi {
   @PostMapping("/login")
   ResponseEntity<ApiResponseDTO<LoginResponseDTO>> login(
       @RequestBody @Valid LoginRequestDTO loginRequestDTO);
+
+  @PostMapping("/refresh")
+  ResponseEntity<ApiResponseDTO<RefreshTokenRequestDTO>> refresh(
+      @RequestBody RefreshTokenRequestDTO refreshTokenRequestDTO,
+      HttpServletRequest request, HttpServletResponse response
+  );
+
+  @PostMapping("/logout")
+  ApiResponseDTO<Void> logout(
+      @RequestBody LogoutRequestDTO logoutRequestDTO,
+      @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+      HttpServletResponse response
+  );
 }
